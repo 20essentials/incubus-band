eb623e0d164e5da70fc4fda14293ebf4a6ccd1bd
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:26:36 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'INCUBUS';
+export let nameSongs = [
+	"DRIVE", /*1*/
+	"WISH YOU WERE HERE", /*2*/
+	"DIG", /*3*/
+	"ANNA MOLLY", /*4*/
+	"LOVE HURTS", /*5*/
+	"PARDON ME", /*6*/
+	"WARNING", /*7*/
+	"ECHO", /*8*/
+	"11 AM", /*9*/
+	"THE WARMTH", /*10*/
+	"JUST A PHASE", /*11*/
+	"BLOOD ON THE GROUND", /*12*/
+	"MEGALOMANIAC", /*13*/
+	"A CROW LEFT OF THE MURDER", /*14*/
+	"HERE IN MY ROOM", /*15*/
+	"ADOLESCENTS", /*16*/
+	"IN THE COMPANY OF WOLVES", /*17*/
+	"THE ORIGINAL", /*18*/
+	"PROMISES, PROMISES", /*19*/
+	"DEFIANCE", /*20*/
+]
